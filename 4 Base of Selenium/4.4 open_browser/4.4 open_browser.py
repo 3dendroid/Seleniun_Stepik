9035fc64ee70a573e2fee9cb88ee9a1a31cf6d25
3dendroid <denkingbad@gmail.com>
2024-05-12 19:40:07 +0700

    Finally, figure out why chromedriver.exe is not working. The problem was in regedit in windows, link of solution: https://stackoverflow.com/questions/52760842/selenium-doesnt-open-the-specified-url-and-shows-data/52761591#52761591

--- 4 Base of Selenium/4.4 open_browser/4.4 open_browser.py
+++ 4 Base of Selenium/4.4 open_browser/4.4 open_browser.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-g = Service()
-driver = webdriver.Chrome(options=options, service=g)
-base_url = 'https://www.saucedemo.com/'
-driver.get(base_url)
+s = Service()
+driver = webdriver.Chrome(options=options, service=s)
 driver.maximize_window()
+driver.get('https://www.saucedemo.com/')
 
 #code is not working
---
+++ 4 Base of Selenium/4.4 open_browser/4.5 send_keys.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+# Options
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+# Service
+s = Service()
+
+# Driver
+driver = webdriver.Chrome(options=options, service=s)
+driver.maximize_window()
+driver.get('https://www.saucedemo.com/')
+
+# Elements
+username = driver.find_element(By.ID, 'user-name')
+username.send_keys('standard_user')
+time.sleep(3)
+
+password = driver.find_element(By.ID, 'password')
+password.send_keys('secret_sauce')
+time.sleep(3)
+
+button = driver.find_element(By.ID, 'login-button')
+button.click()
+time.sleep(3)
+
+# Quit
+driver.quit()
