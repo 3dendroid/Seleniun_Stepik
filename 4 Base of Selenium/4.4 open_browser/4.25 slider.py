ecf3e58e62aade5f1f4dc2653ff6e71ab9ad7d34
denis.sazonov <denis.sazonov@mytona.com>
2024-07-16 16:06:06 +0700

    next 4.26 drop_down.py

--- 4 Base of Selenium/4.4 open_browser/4.25 slider.py
+++ 4 Base of Selenium/4.4 open_browser/4.25 slider.py
@@ -1,7 +1,9 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 # OPTIONS
 options = webdriver.ChromeOptions()
@@ -14,12 +16,24 @@ s = Service()
 # DRIVER
 driver = webdriver.Chrome(options=options, service=s)
 driver.maximize_window()
-driver.get('https://demoqa.com/date-picker')
+driver.get('https://html5css.ru/howto/howto_js_rangeslider.php')
 time.sleep(2)
 
 # SLIDER
-# continue
+actions = ActionChains(driver)
+slider_1 = driver.find_element(By.XPATH, '//input[@type="range"]')
+actions.click_and_hold(slider_1).move_by_offset(50, 0).release().perform()
+print("Slided")
 
+time.sleep(2)
+slider_2 = driver.find_element(By.XPATH, '//input[@id="id2"]')
+actions.click_and_hold(slider_2).move_by_offset(40, 0).release().perform()
+print("Slided")
+
+time.sleep(2)
+slider_3 = driver.find_element(By.XPATH, '//input[@id="id1"]')
+actions.click_and_hold(slider_3).move_by_offset(-70, 0).release().perform()
+print("Slided")
 
 # QUIT
 driver.quit()
