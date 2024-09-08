a07348b121b116eca7f021619dd50df8b637b10c
denis.sazonov <denis.sazonov@mytona.com>
2024-09-08 13:00:27 +0700

    4.36 test_3.py was finished!

--- 4 Base of Selenium/4.4 open_browser/4.36 test_3.py
+++ 4 Base of Selenium/4.4 open_browser/4.36 test_3.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 from selenium import webdriver
@@ -5,206 +6,140 @@ from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# OPTIONS
-options = webdriver.ChromeOptions()
-options.add_experimental_option('detach', True)
-# options.add_argument('--headless')  # headless mode
+from items import *
 
-# SERVICE
-s = Service()
-
-# DRIVER
-driver = webdriver.Chrome(options=options, service=s)
-driver.maximize_window()
-driver.get('https://www.saucedemo.com/')
-
-# ITEMS
-items = {'1': 'Sauce Labs Backpack',
-         '2': 'Sauce Labs Bike Light',
-         '3': 'Sauce Labs Onesie',
-         '4': 'Test.allTheThings() T-Shirt',
-         '5': 'Fleece Jacket',
-         '6': 'Brown T-Shirt'}
+# ITEMS IMPORTED FROM items.py
 
 # ELEMENTS
 login_standard_user = 'standard_user'
 password_all = 'secret_sauce'
-product = ''
 
+print("Приветствую тебя в нашем интернет магазине!")
+print(
+    "Выбери один из следующих товаров и укажи его номер: \n1 - Sauce Labs Backpack \n2 - Sauce Labs Bike Light \n3 - Sauce Labs Onesie \n4 - Test.allTheThings() T-Shirt \n5 - Fleece Jacket \n6 - Brown T-Shirt")
+product = input()
 
-# INPUTS
-def asking():
-    print("Приветствую тебя в нашем интернет магазине")
-    print("Выбери один из следующих товаров и укажи его номер:"
-          "1 - Sauce Labs Backpack,"
-          "2 - Sauce Labs Bike Light,"
-          "3 - Sauce Labs Onesie,"
-          "4 - Test.allTheThings() T-Shirt,"
-          "5 - Fleece Jacket,"
-          "6 - Brown T-Shirt")
+# CHECK IF PRODUCT NUMBER IS DIGIT AND VALID
+if product.isdigit() and int(product) >= 7:
+    print("Такого товара нет. Проверьте корректность ввода.")
+    sys.exit()
 
-    product = input()
-    if product in items:
-        print(f"Вы выбрали {items[product]}. Товар будет куплен.")
-    else:
-        print("Такого товара нет.")
-    return product
+if product in items:
+    print(f"Вы выбрали {items[product]}. Товар будет куплен.")
+else:
+    print("Такого товара нет. Проверьте корректность ввода.")
+    sys.exit()
 
 
 def main():
-    # USERNAME
+    # OPTIONS
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    # options.add_argument('--headless')  # headless mode
+
+    # SERVICE
+    s = Service()
+
+    # DRIVER
+    driver = webdriver.Chrome(options=options, service=s)
+    driver.maximize_window()
+    driver.get('https://www.saucedemo.com/')
+    time.sleep(2)
+
+    # INPUT LOGIN AND PASSWORD
     username = driver.find_element(By.ID, 'user-name')  # by ID attribute
     username.send_keys(login_standard_user)
+    print("INPUT LOGIN")
     time.sleep(2)
-    print("Input Login")
 
-    # PASSWORD
     password = driver.find_element(By.XPATH, '//*[@id="password"]')  # by XPATH attribute
     password.send_keys(password_all)
-    time.sleep(2)
-    print("Input Password")
     password.send_keys(Keys.ENTER)
+    print("INPUT PASSWORD")
     time.sleep(2)
 
     # FIND, SELECT, ADD TO CART
-    # Item 1
-    value_product_1 = driver.find_element(By.XPATH, "//*[@id='item_4_title_link']/div")
-    value_product_1 = value_product_1.text
-    print(value_product_1)
+    if product in xpathes:
+        xpath = xpathes[product]
+        value_product = driver.find_element(By.XPATH, xpath)
+        value_product = value_product.text
+        print(value_product)
+        time.sleep(2)
 
-    value_price_product_1 = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
-    value_price_product_1 = value_price_product_1.text
-    print(value_price_product_1)
+    if product in value_prices:
+        value = value_prices[product]
+        value_price_product = driver.find_element(By.XPATH, value)
+        value_price_product = value_price_product.text
+        print(value_price_product)
+        time.sleep(2)
 
-    select_product_1 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
-    select_product_1.click()
-    time.sleep(2)
-
-    # Item 2
-    value_product_2 = driver.find_element(By.XPATH, "//*[@id='item_0_title_link']/div")
-    value_product_2 = value_product_2.text
-    print(value_product_2)
-
-    value_price_product_2 = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[2]/div[2]/div[2]/div")
-    value_price_product_2 = value_price_product_2.text
-    print(value_price_product_2)
-
-    select_product_2 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bike-light']")
-    select_product_2.click()
-    time.sleep(2)
-
-    # CART
-    cart = driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']")
-    cart.click()
-    print("Enter cart")
-    time.sleep(2)
+    if product in ids:
+        id = ids[product]
+        select_product = driver.find_element(By.XPATH, f"//button[@id='{id}']")
+        select_product.click()
+        print("Select product")
+        time.sleep(2)
 
     # CART INFO, CHECK VALUE AND PRICE
-    # Item 1
-    cart_value_product_1 = driver.find_element(By.XPATH, "//*[@id='item_4_title_link']/div")
-    cart_value_product_1 = cart_value_product_1.text
-    print(cart_value_product_1)
-    assert value_product_1 == cart_value_product_1
-    print("CART VALUE INFO IS GOOD")
-
-    cart_price_product_1 = driver.find_element(By.XPATH,
-                                               "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div")
-    cart_price_product_1 = cart_price_product_1.text
-    print(cart_price_product_1)
-    assert value_price_product_1 == cart_price_product_1
-    print("CART PRICE INFO IS GOOD")
-
-    # Item 2
-    cart_value_product_2 = driver.find_element(By.XPATH, "//*[@id='item_0_title_link']/div")
-    cart_value_product_2 = cart_value_product_2.text
-    print(cart_value_product_2)
-    assert value_product_2 == cart_value_product_2
-    print("CART VALUE INFO IS GOOD")
-
-    cart_price_product_2 = driver.find_element(By.XPATH,
-                                               "//*[@id='cart_contents_container']/div/div[1]/div[4]/div[2]/div[2]/div")
-    cart_price_product_2 = cart_price_product_2.text
-    print(cart_price_product_2)
-    assert value_price_product_2 == cart_price_product_2
-    print("CART PRICE INFO IS GOOD")
-
-    # CHECKOUT
-    checkout = driver.find_element(By.XPATH, "//button[@id='checkout']")
-    checkout.click()
-    print("Click checkout")
+    cart = driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']")
+    cart.click()
+    print("ENTER CART")
     time.sleep(2)
 
-    # INPUT DATA
+    if product in cart_xpathes:
+        cart_id = cart_xpathes[product]
+        cart_xpath = f"//*[@id='{cart_id}']/div"
+        cart_value_product = driver.find_element(By.XPATH, cart_xpath)
+        cart_value_product = cart_value_product.text
+        print(cart_value_product)
+        time.sleep(2)
+
+    assert value_product == cart_value_product
+    print("CART VALUE INFO IS GOOD")
+    cart_price_product = driver.find_element(By.XPATH,
+                                             "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div")
+    cart_price_product = cart_price_product.text
+    print(cart_price_product)
+
+    assert value_price_product == cart_price_product
+    print("CART PRICE INFO IS GOOD")
+    time.sleep(2)
+
+    # CHECKOUT, INPUT DATA
+    checkout = driver.find_element(By.XPATH, "//button[@id='checkout']")
+    checkout.click()
+    print("CLICK CHECKOUT")
+    time.sleep(2)
+
     first_name = driver.find_element(By.XPATH, "//*[@id='first-name']")
     first_name.send_keys('Denis')
-    print("Input first name")
+    print("INPUT FIRST NAME")
+    time.sleep(2)
 
     last_name = driver.find_element(By.XPATH, "//*[@id='last-name']")
     last_name.send_keys('Sazonov')
-    print("Input last name")
+    print("INPUT LAST NAME")
+    time.sleep(2)
 
     postal_code = driver.find_element(By.XPATH, "//*[@id='postal-code']")
     postal_code.send_keys('100500')
-    print("Input ZIP")
+    print("INPUT ZIP")
     time.sleep(2)
 
     continue_button = driver.find_element(By.XPATH, "//input[@id='continue']")
     continue_button.click()
-    time.sleep(2)
 
     # CART INFO, CHECK VALUE AND PRICE
-    # Item 1
-    finish_value_product_1 = driver.find_element(By.XPATH, "//*[@id='item_4_title_link']/div")
-    finish_value_product_1 = finish_value_product_1.text
-    print(finish_value_product_1)
-    assert value_product_1 == finish_value_product_1
+    finish_value_product = driver.find_element(By.XPATH, xpath)
+    finish_value_product = finish_value_product.text
+    print(finish_value_product)
+    assert value_product == finish_value_product
     print("FINAL VALUE INFO IS GOOD")
-
-    # Item 2
-    finish_value_product_2 = driver.find_element(By.XPATH, "//*[@id='item_0_title_link']/div")
-    finish_value_product_2 = finish_value_product_2.text
-    print(finish_value_product_2)
-    assert value_product_2 == finish_value_product_2
-    print("FINAL VALUE INFO IS GOOD")
-
-    # Finish price of 2 products
-    finish_price_product_1 = driver.find_element(By.XPATH,
-                                                 "//*[@id='checkout_summary_container']/div/div[1]/div[3]/div[2]/div[2]/div")
-    finish_price_product_1 = finish_price_product_1.text
-    print(finish_price_product_1)
-    assert value_price_product_1 == finish_price_product_1
-    print("FINAL PRICE INFO IS GOOD")
-
-    finish_price_product_2 = driver.find_element(By.XPATH,
-                                                 "//*[@id='checkout_summary_container']/div/div[1]/div[4]/div[2]/div[2]/div")
-    finish_price_product_2 = finish_price_product_2.text
-    print(finish_price_product_2)
-    assert value_price_product_2 == finish_price_product_2
-    print("FINAL PRICE INFO IS GOOD")
-
-    # SEPARATE $ AND COMPARE item_total with value_summary_price
-    summary_price = driver.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[2]/div[6]")
-    value_summary_price = summary_price.text
-    print(value_summary_price)
-
-    float_value_price_product_1 = float(value_price_product_1.replace('$', ''))
-    print(float_value_price_product_1)
-
-    float_value_price_product_2 = float(value_price_product_2.replace('$', ''))
-    print(float_value_price_product_2)
-
-    float_summary = float_value_price_product_1 + float_value_price_product_2
-    print(float_summary)
-
-    item_total = "Item total: " + "$" + str(float_summary)
-    print(item_total)
-    assert value_summary_price == str(item_total)
-    print("SUCCESS!")
+    time.sleep(2)
 
     # QUIT
     driver.quit()
 
 
 if __name__ == "__main__":
-    asking()
     main()
--- 4 Base of Selenium/4.4 open_browser/test.py
+++ 4 Base of Selenium/4.4 open_browser/test.py
@@ -1,52 +1,34 @@
+import sys
+import time
+
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# ITEMS
-items = {'1': 'Sauce Labs Backpack',
-         '2': 'Sauce Labs Bike Light',
-         '3': 'Sauce Labs Bolt T-Shirt',
-         '4': 'Sauce Labs Fleece Jacket',
-         '5': 'Sauce Labs Onesie',
-         '6': 'Test.allTheThings() T-Shirt (Red)'}
-xpathes = {
-    '1': "//*[@id='item_4_title_link']/div",
-    '2': "//*[@id='item_0_title_link']/div",
-    '3': "//*[@id='item_1_title_link']/div",
-    '4': "//*[@id='item_5_title_link']/div",
-    '5': "//*[@id='item_2_title_link']/div",
-    '6': "//*[@id='item_3_title_link']/div"
-}
-ids = {'1': 'add-to-cart-sauce-labs-backpack',
-       '2': 'add-to-cart-sauce-labs-bike-light',
-       '3': 'add-to-cart-sauce-labs-bolt-t-shirt',
-       '4': 'add-to-cart-sauce-labs-fleece-jacket',
-       '5': 'add-to-cart-sauce-labs-onesie',
-       '6': 'add-to-cart-test.allthethings()-t-shirt-(red)'
-       }
+from items import *
 
-cart_xpathes = {'1': 'item_4_title_link',
-                '2': 'item_0_title_link',
-                '3': 'item_1_title_link',
-                '4': 'item_5_title_link',
-                '5': 'item_2_title_link',
-                '6': 'item_3_title_link'
-                }
+# ITEMS IMPORTED FROM items.py
 
 # ELEMENTS
 login_standard_user = 'standard_user'
 password_all = 'secret_sauce'
 
-print("Приветствую тебя в нашем интернет магазине")
+print("Приветствую тебя в нашем интернет магазине!")
 print(
-    "Выбери один из следующих товаров и укажи его номер: 1 - Sauce Labs Backpack, 2 - Sauce Labs Bike Light, 3 - Sauce Labs Onesie, 4 - Test.allTheThings() T-Shirt, 5 - Fleece Jacket, 6 - Brown T-Shirt")
+    "Выбери один из следующих товаров и укажи его номер: \n1 - Sauce Labs Backpack \n2 - Sauce Labs Bike Light \n3 - Sauce Labs Onesie \n4 - Test.allTheThings() T-Shirt \n5 - Fleece Jacket \n6 - Brown T-Shirt")
 product = input()
+
+# CHECK IF PRODUCT NUMBER IS DIGIT AND VALID
+if product.isdigit() and int(product) >= 7:
+    print("Такого товара нет. Проверьте корректность ввода.")
+    sys.exit()
+
 if product in items:
-    choice = items[product]
     print(f"Вы выбрали {items[product]}. Товар будет куплен.")
 else:
-    print("Такого товара нет.")
+    print("Такого товара нет. Проверьте корректность ввода.")
+    sys.exit()
 
 
 def main():
@@ -62,16 +44,19 @@ def main():
     driver = webdriver.Chrome(options=options, service=s)
     driver.maximize_window()
     driver.get('https://www.saucedemo.com/')
+    time.sleep(2)
 
     # INPUT LOGIN AND PASSWORD
     username = driver.find_element(By.ID, 'user-name')  # by ID attribute
     username.send_keys(login_standard_user)
-    print("Input Login")
+    print("INPUT LOGIN")
+    time.sleep(2)
 
     password = driver.find_element(By.XPATH, '//*[@id="password"]')  # by XPATH attribute
     password.send_keys(password_all)
-    print("Input Password")
     password.send_keys(Keys.ENTER)
+    print("INPUT PASSWORD")
+    time.sleep(2)
 
     # FIND, SELECT, ADD TO CART
     if product in xpathes:
@@ -79,21 +64,27 @@ def main():
         value_product = driver.find_element(By.XPATH, xpath)
         value_product = value_product.text
         print(value_product)
-        value_price_product = driver.find_element(By.XPATH,
-                                                  "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
+        time.sleep(2)
+
+    if product in value_prices:
+        value = value_prices[product]
+        value_price_product = driver.find_element(By.XPATH, value)
         value_price_product = value_price_product.text
         print(value_price_product)
+        time.sleep(2)
 
     if product in ids:
         id = ids[product]
         select_product = driver.find_element(By.XPATH, f"//button[@id='{id}']")
         select_product.click()
         print("Select product")
+        time.sleep(2)
 
     # CART INFO, CHECK VALUE AND PRICE
     cart = driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']")
     cart.click()
-    print("Enter cart")
+    print("ENTER CART")
+    time.sleep(2)
 
     if product in cart_xpathes:
         cart_id = cart_xpathes[product]
@@ -101,34 +92,39 @@ def main():
         cart_value_product = driver.find_element(By.XPATH, cart_xpath)
         cart_value_product = cart_value_product.text
         print(cart_value_product)
+        time.sleep(2)
 
     assert value_product == cart_value_product
     print("CART VALUE INFO IS GOOD")
     cart_price_product = driver.find_element(By.XPATH,
                                              "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div")
     cart_price_product = cart_price_product.text
-    new_cart_price_product = cart_price_product
     print(cart_price_product)
 
     assert value_price_product == cart_price_product
     print("CART PRICE INFO IS GOOD")
+    time.sleep(2)
 
     # CHECKOUT, INPUT DATA
     checkout = driver.find_element(By.XPATH, "//button[@id='checkout']")
     checkout.click()
-    print("Click checkout")
+    print("CLICK CHECKOUT")
+    time.sleep(2)
 
     first_name = driver.find_element(By.XPATH, "//*[@id='first-name']")
     first_name.send_keys('Denis')
-    print("Input first name")
+    print("INPUT FIRST NAME")
+    time.sleep(2)
 
     last_name = driver.find_element(By.XPATH, "//*[@id='last-name']")
     last_name.send_keys('Sazonov')
-    print("Input last name")
+    print("INPUT LAST NAME")
+    time.sleep(2)
 
     postal_code = driver.find_element(By.XPATH, "//*[@id='postal-code']")
     postal_code.send_keys('100500')
-    print("Input ZIP")
+    print("INPUT ZIP")
+    time.sleep(2)
 
     continue_button = driver.find_element(By.XPATH, "//input[@id='continue']")
     continue_button.click()
@@ -139,6 +135,7 @@ def main():
     print(finish_value_product)
     assert value_product == finish_value_product
     print("FINAL VALUE INFO IS GOOD")
+    time.sleep(2)
 
     # QUIT
     driver.quit()
