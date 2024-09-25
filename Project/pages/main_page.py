6fd160d1eaf3e79fa5bbd437ceca6713083fb72c
denis.sazonov <denis.sazonov@mytona.com>
2024-09-25 11:52:51 +0700

    next 7.12

--- Project/pages/main_page.py
+++ Project/pages/main_page.py
@@ -12,26 +12,76 @@ class Main_page(Base):
         self.driver = driver
 
     # LOCATORS
-    select_product_1 = '//button[@id="add-to-cart-sauce-labs-backpack"]'
+    product_1 = '//button[@id="add-to-cart-sauce-labs-backpack"]'
+    product_2 = '//div[normalize-space()="Sauce Labs Bike Light"]'
+    product_3 = '//div[normalize-space()="Sauce Labs Bolt T-Shirt"]'
     cart = '//div[@id="shopping_cart_container"]'
+    menu = '//button[@id="react-burger-menu-btn"]'
+    link_about = '//a[@id="about_sidebar_link"]'
 
     # GETTERS
     def get_select_product_1(self):
-        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.select_product_1)))
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.product_1)))
+
+    def get_select_product_2(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.product_2)))
+
+    def get_select_product_3(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.product_3)))
 
     def get_cart(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.cart)))
+
+    def get_menu(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.menu)))
+
+    def get_link_about(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.link_about)))
 
     # ACTIONS
     def click_select_product_1(self):
         self.get_select_product_1().click()
         print("CLICK SELECT PRODUCT 1")
 
+    def click_select_product_2(self):
+        self.get_select_product_2().click()
+        print("CLICK SELECT PRODUCT 2")
+
+    def click_select_product_3(self):
+        self.get_select_product_3().click()
+        print("CLICK SELECT PRODUCT 3")
+
     def click_cart(self):
         self.get_cart().click()
         print("CLICK CART")
 
-    def select_product(self):
+    def click_menu(self):
+        self.get_menu().click()
+        print("CLICK MENU")
+
+    def click_link_about(self):
+        self.get_link_about().click()
+        print("CLICK LINK ABOUT")
+
+    # METHODS
+    def select_menu_about(self):
         self.get_current_url()
-        self.click_select_product_1()  # select product (7.6)
+        self.click_menu()
+        self.click_link_about()  # second test (7.10)
+        self.assert_url('https://saucelabs.com/')
+        self.get_screenshot()
+
+    def select_product_1(self):
+        self.get_current_url()
+        self.click_select_product_1()
+        self.click_cart()
+
+    def select_product_2(self):
+        self.get_current_url()
+        self.click_select_product_2()
+        self.click_cart()
+
+    def select_product_3(self):
+        self.get_current_url()
+        self.click_select_product_3()  # select product (7.6)
         self.click_cart()
--- Project/tests/test_buy_product.py
+++ Project/tests/test_buy_product.py
@@ -1,52 +1,109 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 from Project.pages.cart_page import Cart_page
-from Project.pages.client_informaton_page import Client_information_page
-from Project.pages.finish_page import Finish_page
 from Project.pages.login_page import Login_page
 from Project.pages.main_page import Main_page
-from Project.pages.payment_page import Payment_page
 
 
-class Test_5:
+@pytest.mark.order(3)
+def test_buy_product_1():
+    # OPTIONS
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # clear console (7.3)
 
-    def test_buy_product(self):
-        print('TEST IS STARTED!')
+    # SERVICE
+    s = Service()
 
-        # OPTIONS
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option('detach', True)
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])  # clear console (7.3)
+    # DRIVER
+    driver = webdriver.Chrome(options=options, service=s)
 
-        # SERVICE
-        s = Service()
+    # LOGIN, PASSWORD AND CLICK LOGIN BUTTON
+    print('TEST 1 IS STARTED!')  # some tests (7.11)
 
-        # DRIVER
-        driver = webdriver.Chrome(options=options, service=s)
+    login = Login_page(driver)
+    login.authorization()
 
-        # LOGIN, PASSWORD AND CLICK LOGIN BUTTON
-        print('TEST IS STARTED!')
+    mp = Main_page(driver)
+    mp.select_product_1()
+    mp.get_current_url()
 
-        login = Login_page(driver)
-        login.authorization()
+    cp = Cart_page(driver)
+    cp.product_confirmation()
 
-        mp = Main_page(driver)
-        mp.select_product()
-        mp.get_current_url()
+    # cip = Client_information_page(driver)
+    # cip.input_information()
+    #
+    # pp = Payment_page(driver)
+    # pp.payment()
+    #
+    # f = Finish_page(driver)
+    # f.get_screenshot()
 
-        cp = Cart_page(driver)
-        cp.product_confirmation()
+    # DRIVER QUIT
+    print('TEST 1 IS OVER!')
+    driver.quit()
 
-        cip = Client_information_page(driver)
-        cip.input_information()
 
-        pp = Payment_page(driver)
-        pp.payment()
+@pytest.mark.order(1)
+def test_buy_product_2():  # some tests (7.11)
+    # OPTIONS
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # clear console (7.3)
 
-        f = Finish_page(driver)
-        f.get_screenshot()
+    # SERVICE
+    s = Service()
 
-        # DRIVER QUIT
-        print('TEST IS OVER!')
-        driver.quit()
+    # DRIVER
+    driver = webdriver.Chrome(options=options, service=s)
+
+    # LOGIN, PASSWORD AND CLICK LOGIN BUTTON
+    print('TEST 2 IS STARTED!')
+
+    login = Login_page(driver)
+    login.authorization()
+
+    mp = Main_page(driver)
+    mp.select_product_2()
+    mp.get_current_url()
+
+    cp = Cart_page(driver)
+    cp.product_confirmation()
+
+    # DRIVER QUIT
+    print('TEST 2 IS OVER!')
+    driver.quit()
+
+
+@pytest.mark.order(2)
+def test_buy_product_3():  # some tests (7.11)
+    # OPTIONS
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # clear console (7.3)
+
+    # SERVICE
+    s = Service()
+
+    # DRIVER
+    driver = webdriver.Chrome(options=options, service=s)
+
+    # LOGIN, PASSWORD AND CLICK LOGIN BUTTON
+    print('TEST 3 IS STARTED!')
+
+    login = Login_page(driver)
+    login.authorization()
+
+    mp = Main_page(driver)
+    mp.select_product_3()
+    mp.get_current_url()
+
+    cp = Cart_page(driver)
+    cp.product_confirmation()
+
+    # DRIVER QUIT
+    print('TEST 3 IS OVER!')
+    driver.quit()
